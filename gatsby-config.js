c65807cb439d6fd87fba351f709edd6b314cb934
khongchai <world1955@hotmail.com>
2020-12-14 22:57:54 +0700

    finished setting up sharp and mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,45 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    //plugins for dealing with image optimization so that pages can be created dynamically.
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extension: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            option: {
+              maxWidth: 1200,
+            }
+          }
+        ]
+        
+      }
+    },
+  ],
+  
 }
